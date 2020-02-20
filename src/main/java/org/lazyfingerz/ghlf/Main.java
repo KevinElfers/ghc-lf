35d5494d8c46dc952d4d5a0be326d4d206a873e4
Daniel Teske <daniel.teske@europace.de>
2020-02-20 21:43:25 +0100

    iterate through all files

--- src/main/java/org/lazyfingerz/ghlf/Main.java
+++ src/main/java/org/lazyfingerz/ghlf/Main.java
@@ -7,23 +7,30 @@ import org.lazyfingerz.ghlf.model.LfResult;
 import org.lazyfingerz.ghlf.processor.LfProcessor;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
 
-    private final static String A = "a_example.txt";
-    private final static String B = "b_read_on.txt";
-    private final static String C = "c_incunabula.txt";
-    private final static String D = "d_tough_choices.txt";
-    private final static String E = "e_so_many_books.txt";
-    private final static String F = "f_libraries_of_the_world.txt";
+  private final static String A = "a_example.txt";
+  private final static String B = "b_read_on.txt";
+  private final static String C = "c_incunabula.txt";
+  private final static String D = "d_tough_choices.txt";
+  private final static String E = "e_so_many_books.txt";
+  private final static String F = "f_libraries_of_the_world.txt";
 
-    public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
 
-        String filename = F;
-
+    Arrays.asList(A, B, C, D, E, F).forEach(filename -> {
+      try {
+        System.out.println("Start " + filename);
         LfProblemInstance problemInstance = new LfReader().read("src/main/resources/" + filename);
         LfResult result = new LfProcessor().process(problemInstance);
         new LfWriter().write(result, filename);
-    }
-
+        System.out.println("Done!");
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+      }
+    });
+  }
 }
