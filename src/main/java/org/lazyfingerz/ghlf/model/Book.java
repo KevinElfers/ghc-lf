4ac915a10e9d9a995f87d81a5187df4f97bbd914
Kevin Elfers <kev_1992@web.de>
2020-02-20 19:26:58 +0100

    add book comparator

--- src/main/java/org/lazyfingerz/ghlf/model/Book.java
+++ src/main/java/org/lazyfingerz/ghlf/model/Book.java
@@ -3,12 +3,19 @@ package org.lazyfingerz.ghlf.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Comparator;
+
 @Data
 @AllArgsConstructor
-public class Book {
+public class Book implements Comparable<Book> {
 
     private int index;
 
     private int value;
 
+
+    @Override
+    public int compareTo(Book book) {
+        return Comparator.comparingInt(Book::getValue).compare(this, book);
+    }
 }
