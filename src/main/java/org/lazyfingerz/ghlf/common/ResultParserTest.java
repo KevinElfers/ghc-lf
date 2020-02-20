623f542c53ad33a48d4146ade4efbd982ce112f3
Daniel Teske <daniel.teske@europace.de>
2020-02-20 20:31:31 +0100

    test parseUniqueOrderedLibraryIds

--- src/main/java/org/lazyfingerz/ghlf/common/ResultParserTest.java
+++ src/main/java/org/lazyfingerz/ghlf/common/ResultParserTest.java
@@ -6,22 +6,36 @@ import org.lazyfingerz.ghlf.model.BookPackage;
 import org.lazyfingerz.ghlf.model.LfResult;
 import org.lazyfingerz.ghlf.model.Library;
 
+import java.util.List;
+
 import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
 
 public class ResultParserTest {
 
   @Test
-  public void test() {
+  public void test_parseUniqueOrderedLibraryIds() {
+    //given
     LfResult result = new LfResult();
     Book b1 = new Book(0, 0);
     Book b2 = new Book(1, 1);
     Book b3 = new Book(2, 2);
     Book b4 = new Book(3, 3);
-    Library lib1 = new Library();
-    lib1.setBooks(asList(b1, b2));
-    Library lib2 = new Library();
-    lib2.setBooks(asList(b3, b4));
+    Library lib2 = new Library(1, null, null, asList(b3, b4));
+    Library lib1 = new Library(0, null, null, asList(b1, b2));
 
-    result.add(new BookPackage(lib1, asList(b1)));
+    result.add(new BookPackage(lib1, asList(b1, b2)));
+    result.add(new BookPackage(lib2, asList(b3, b4)));
+    result.add(new BookPackage(lib1, asList(b3, b4)));
+    result.add(new BookPackage(lib2, asList(b3, b4)));
+
+    //when
+    ResultParser parser = new ResultParser(result);
+    List<String> libraryIds = parser.parseUniqueOrderedLibraryIds();
+
+    //then
+    assertEquals(libraryIds.size(), 2);
+    assertEquals(libraryIds.get(0), "0");
+    assertEquals(libraryIds.get(1), "1");
   }
 }
