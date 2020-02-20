a292f8d6162eb1464b7a584a030256d7fe5a7f06
Kevin Elfers <kev_1992@web.de>
2020-02-20 20:10:57 +0100

    first draft processor

--- src/main/java/org/lazyfingerz/ghlf/processor/LfProcessor.java
+++ src/main/java/org/lazyfingerz/ghlf/processor/LfProcessor.java
@@ -1,14 +1,43 @@
 package org.lazyfingerz.ghlf.processor;
 
-import org.lazyfingerz.ghlf.model.LfProblemInstance;
-import org.lazyfingerz.ghlf.model.LfResult;
+import org.lazyfingerz.ghlf.model.*;
+
+import java.util.*;
 
 public class LfProcessor {
 
     public LfResult process(LfProblemInstance problemInstance) {
-        return new LfResult(problemInstance.getInput());
+
+        List<Book> alreadyUsedBooks = new ArrayList<>();
+        List<BookPackage> allBookPackages = new ArrayList<>();
+        List<Library> SubscribedLibraries;
+
+        int day = 0;
+
+        while (day < problemInstance.getDays()) {
+            day--;
+            TreeSet<Library> libraries = new TreeSet<>(new LibraryComparator(alreadyUsedBooks));
+
+            //create bookpackages for current day from all subscribed libraries;
+            HashSet<BookPackage> newBookPackages = new HashSet<>();
+            BookPackage newBookPackage;
+
+            // remove new packages from all libraries
+            for (Library library : libraries) {
+                for (BookPackage bookPackage : newBookPackages) {
+                    library.getBooks().removeAll(bookPackage.getBooks());
+                }
+            }
+
+            // add new packages to result
+            allBookPackages.addAll(newBookPackages);
+        }
+
+
+        return new LfResult(allBookPackages);
     }
 
 
 
 }
+
