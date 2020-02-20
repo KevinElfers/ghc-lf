b21070a9132931a749c792527b7257bf90795d9e
Kevin Elfers <kev_1992@web.de>
2020-02-20 20:11:59 +0100

    fix result constructor

--- src/main/java/org/lazyfingerz/ghlf/model/LfResult.java
+++ src/main/java/org/lazyfingerz/ghlf/model/LfResult.java
@@ -1,5 +1,6 @@
 package org.lazyfingerz.ghlf.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class LfResult {
 
   List<BookPackage> bookPackages = new ArrayList<>();
